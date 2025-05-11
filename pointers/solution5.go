08b0bed4e387f3268bd4982d315066efcb22ca6a
Parth Maru <theparthmaru@gmail.com>
2025-05-11 20:27:36 +0100

    Added solution 5 for pointer

---
+++ pointers/solution5.go
@@ -0,0 +1,25 @@
+package pointersSolutions
+
+import "fmt"
+
+func Solution5() {
+	num1 := -9
+	num2 := -3
+
+	fmt.Println(minMax(&num1, &num2))
+}
+
+func minMax(num1, num2 *int) (int, int) {
+	larger := 0
+	smaller := 0
+
+	if *num1 >= *num2 {
+		larger = *num1
+		smaller = *num2
+	} else {
+		larger = *num2
+		smaller = *num1
+	}
+
+	return larger, smaller
+}
