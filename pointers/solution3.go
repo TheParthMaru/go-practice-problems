f0ac9c1f105f7fe00a041368afd5992894bb36e3
Parth Maru <theparthmaru@gmail.com>
2025-05-11 14:40:34 +0100

    Added solution 3

---
+++ pointers/solution3.go
@@ -0,0 +1,18 @@
+package pointersSolutions
+
+import "fmt"
+
+func Solution3() {
+	x := 10
+	y := 69
+
+	swap(&x, &y)
+
+	fmt.Println(x, y)
+}
+
+func swap(a, b *int) {
+	temp := *a
+	*a = *b
+	*b = temp
+}
